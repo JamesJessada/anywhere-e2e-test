625434fd8d5a33d81c6c1cc93234a9ba602d80ca
jamesjessada <jessada@skooldio.com>
2023-02-14 14:24:52 +0700

    add baseUrl to e2e test

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,10 +1,12 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+	viewportWidth: 800,
+	viewportHeight: 500,
 	e2e: {
 		setupNodeEvents(on, config) {
 			// implement node event listeners here
 		},
-		// baseUrl: "https://classroom.dev-onlineathome.ondemand.in.th",
+		baseUrl: "https://classroom.dev-onlineathome.ondemand.in.th",
 	},
 });
